ef28cb84e26c42dff66ca05bf19753084f607a2a
Liza Shermayster <novliza86@gmail.com>
2019-09-29 11:58:36 +0300

    added comments

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,107 +1,3 @@
-// piece object
-const piece = (function () {
-  let el = null;
-  let initTop;
-  let initLeft;
-
-  const init = function (el) {
-    this.el = el;
-    const pos = this.el.getBoundingClientRect();
-    initLeft = pos.left;
-    initTop = pos.top;
-  };
-
-  const reset = function () {
-    this.el.style.left = initLeft + "px";
-    this.el.style.top = initTop + "px";
-  }
-  const moveRandom = function () {
-    this.el.style.left = Math.floor(Math.random() * 100) + "vw";
-    this.el.style.top = Math.floor(Math.random() * 100) + "vh";
-  }
-  const moveDelta = function (dx, dy) {
-
-    const pos = this.el.getBoundingClientRect();
-    const screenWidth =
-      window.innerWidth || document.documentElement.clientWidth;
-
-    const screenHeight =
-      window.innerHeight || document.documentElement.clientHeight;
-
-    const newLeft = pos.left + dx;
-    const maxLeft = screenWidth - 100;
-
-    const newLeftPos = () => {
-      if (newLeft > maxLeft) {
-        return maxLeft;
-      } else if (newLeft < 0) {
-        return 0;
-      }
-      return newLeft;
-    }
-
-
-    const newTop = pos.top + dy;
-    const maxTop = screenHeight - 100;
-    const newTopPos = () => {
-      if (newTop > maxTop) {
-        return maxTop;
-      } else if (newTop < 0) {
-        return 0;
-      }
-      return newTop;
-    }
-    const getCurrLeftPos = () => this.el.getBoundingClientRect().left;
-
-    const createAnimationDx = setInterval(function () {
-      const direction = dx > 0 ? 'right' : 'left';
-      const final = newLeftPos();
-      const current = getCurrLeftPos();
-
-      if (direction === 'right' ? current >= final : final >= current) {
-        clearInterval(createAnimationDx);
-        return;
-      }
-      const step = direction === 'right' ? 3 : -3;
-      this.el.style.left = `${current + step}px`;
-    }.bind(this), 20);
-
-
-
-    const getCurrTopPos = () => this.el.getBoundingClientRect().top;
-    const createAnimationDy = setInterval(function () {
-      const direction = dy > 0 ? 'top' : 'down';
-      const final = newTopPos();
-      const current = getCurrTopPos();
-
-      if (direction === 'top' ? current >= final : final >= current) {
-        clearInterval(createAnimationDy);
-        return;
-      }
-      const step = direction === 'top' ? 3 : -3;
-      this.el.style.top = `${current + step}px`;
-    }.bind(this), 20)
-
-    // this.el.style.left = `${newLeftPos()}px`;
-    // this.el.style.top = `${newTopPos()}px`;
-  };
-
-  const setColor = function (color) {
-    this.el.classList.add(color);
-  }
-  return {
-    init,
-    reset,
-    moveRandom,
-    moveDelta,
-    setColor
-  };
-})();
-
-function handleClick(ev) {
-  piece.moveDelta(parseInt(this.dataset.dx), parseInt(this.dataset.dy));
-}
-
 const config = [
   {
     btnId: 'btn-up',
@@ -125,6 +21,118 @@ const config = [
   }
 ];
 
+// piece object
+const piece = (function () {
+  let el = null;
+  let initTop;
+  let initLeft;
+
+  const setColor = function (color) {
+    this.el.classList.add(color);
+  }
+
+  const init = function (el) {
+    this.el = el;
+    const pos = this.el.getBoundingClientRect();
+    initLeft = pos.left;
+    initTop = pos.top;
+  };
+
+  const reset = function () {
+    this.el.style.left = initLeft + "px";
+    this.el.style.top = initTop + "px";
+  }
+
+  const moveRandom = function () {
+    this.el.style.left = Math.floor(Math.random() * 100) + "vw";
+    this.el.style.top = Math.floor(Math.random() * 100) + "vh";
+  }
+
+  const moveDelta = function (dx, dy) {
+    const pos = this.el.getBoundingClientRect();
+
+    //move on x axis 
+    const getCurrLeftPos = () => this.el.getBoundingClientRect().left;
+    const createAnimationDx = setInterval(() => {
+      const direction = dx > 0 ? 'right' : 'left';
+      const final = getPieceLeftPosition(pos.left, dx);
+      const current = getCurrLeftPos();
+
+      if (direction === 'right' ? current >= final : final >= current) {
+        clearInterval(createAnimationDx);
+        return;
+      }
+      const step = direction === 'right' ? 3 : -3;
+      this.el.style.left = `${current + step}px`;
+    }, 20);
+
+    //move on y axis
+    const getCurrTopPos = () => this.el.getBoundingClientRect().top;
+    const createAnimationDy = setInterval(() => {
+      const direction = dy > 0 ? 'top' : 'down';
+      const final = getPieceRightPosition(pos.top, dy);
+      const current = getCurrTopPos();
+
+      if (direction === 'top' ? current >= final : final >= current) {
+        clearInterval(createAnimationDy);
+        return;
+      }
+      const step = direction === 'top' ? 3 : -3;
+      this.el.style.top = `${current + step}px`;
+    }, 20)
+  };
+
+  return {
+    init,
+    reset,
+    moveRandom,
+    moveDelta,
+    setColor
+  };
+})();
+
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  piece.init(document.getElementById("piece"));
+  initRandomBtn();
+  initResetBtn();
+  initMoveBtns();
+  fetchTemp();
+}
+
+
+function initRandomBtn() {
+  const random = document.getElementById('btn-random');
+  random.addEventListener('click', () => {
+    piece.moveRandom();
+  })
+}
+function initResetBtn() {
+  const reset = document.getElementById("btn-reset");
+  reset.addEventListener('click', resetBtnClicked);
+}
+
+function initMoveBtns() {
+  for (const el of config) {
+    initBtn(el);
+  }
+}
+
+/**
+ * @desc gives color to piece element according to Tel Aviv temperature
+ */
+function fetchTemp() {
+  const telAvivTempUrl = "http://api.apixu.com/v1/current.json?key=dda6e762ae4f41efb7e173552192204&q=tel%20aviv";
+  fetch(telAvivTempUrl)
+    .then(validateResponse)
+    .then(readResponseAsJSON)
+    .then(getTempInC)
+    .then(getColor)
+    .then(piece.setColor.bind(piece));
+}
+
 function initBtn(el) {
   const $btn = document.getElementById(el.btnId);
   $btn.dataset.dx = el.dx;
@@ -132,40 +140,14 @@ function initBtn(el) {
   $btn.addEventListener("click", handleClick);
 }
 
-function init() {
-  for (const el of config) {
-    initBtn(el);
-  }
-}
-
 function resetBtnClicked() {
   piece.reset();
 }
-function initRandBtn(params) {
-  const random = document.getElementById('btn-random');
-  random.addEventListener('click', () => {
-    piece.moveRandom();
-  })
+
+function handleClick(ev) {
+  piece.moveDelta(parseInt(this.dataset.dx), parseInt(this.dataset.dy));
 }
 
-function resetLocation() {
-  const reset = document.getElementById("btn-reset");
-  reset.addEventListener('click', resetBtnClicked);
-}
-
-
-function fetchTemp() {
-
-  fetch("http://api.apixu.com/v1/current.json?key=dda6e762ae4f41efb7e173552192204&q=tel%20aviv")
-    .then(validateResponse)
-    .then(readResponseAsJSON)
-    .then(data => {
-      console.log(data)
-      return data.current.temp_c
-    })
-    .then(getColor)
-    .then(piece.setColor.bind(piece));
-}
 function validateResponse(response) {
   if (!response.ok) {
     throw Error(response.statusText);
@@ -175,6 +157,10 @@ function validateResponse(response) {
 
 function readResponseAsJSON(response) {
   return response.json();
+}
+
+function getTempInC(data) {
+  return data.current.temp_c;
 }
 
 function getColor(tem) {
@@ -191,10 +177,30 @@ function getColor(tem) {
   }
 }
 
-window.addEventListener("DOMContentLoaded", event => {
-  piece.init(document.getElementById("piece"));
-  initRandBtn();
-  resetLocation();
-  init();
-  fetchTemp();
-});
+function getPieceLeftPosition(posLeft, dx) {
+  const screenWidth = window.innerWidth || document.documentElement.clientWidth;
+  const newLeft = posLeft + dx;
+  const maxLeft = screenWidth - 100;
+
+  if (newLeft > maxLeft) {
+    return maxLeft;
+  } else if (newLeft < 0) {
+    return 0;
+  }
+  return newLeft;
+}
+
+function getPieceRightPosition(posRight, dy) {
+  const screenHeight = window.innerHeight || document.documentElement.clientHeight;
+  const newTop = posRight + dy;
+  const maxTop = screenHeight - 100;
+
+  if (newTop > maxTop) {
+    return maxTop;
+  } else if (newTop < 0) {
+    return 0;
+  }
+  return newTop;
+}
+
+
