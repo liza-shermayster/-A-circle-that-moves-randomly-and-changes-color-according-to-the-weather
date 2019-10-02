036920567f79f91f6d818ede411fa2c3b7e6e1cf
Liza Shermayster <novliza86@gmail.com>
2019-10-02 14:22:11 +0300

    Deleted notes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -77,33 +77,5 @@ function handleClick(ev) {
   piece.moveDelta(parseInt(this.dataset.dx), parseInt(this.dataset.dy));
 }
 
-// function validateResponse(response) {
-//   if (!response.ok) {
-//     throw Error(response.statusText);
-//   }
-//   return response;
-// }
-
-// function readResponseAsJSON(response) {
-//   return response.json();
-// }
-
-// function getTempInC(data) {
-//   return data.current.temp_c;
-// }
-
-// function getColor(tem) {
-//   switch (tem) {
-//     case tem <= 10:
-//       return 'blue';
-//     case 10 < tem <= 20:
-//       return 'green';
-//     case 20 < tem <= 30:
-//       return 'yellow';
-//     default:
-//       return "red";
-
-//   }
-// }
 
 
